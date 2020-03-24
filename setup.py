6c4a3a12c3d5e7d1bfcbdbc44aa2fbb73842d254
Shi_Sheng <39020034+ShiSheng233@users.noreply.github.com>
2020-03-24 11:02:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md","r",encoding='utf-8') as fh:
 
 setuptools.setup(
     name="biliBV",
-    version="1.0.0",
+    version="1.0.1",
     author="ShiSheng",
     author_email="shisheng233@dingtalk.com",
     description="A Python Lirary that you can encode av-number to BV-string or decode BV-string.",
@@ -15,5 +15,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ShiSheng233/bili_BV",
     packages=setuptools.find_packages(),
-    python_requires='>=3.5',
+    python_requires='>=3.6',
 )
